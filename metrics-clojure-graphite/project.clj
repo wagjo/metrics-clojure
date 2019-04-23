924d4d34b062b0a718f1c88f25351527e5a0336a
Jozef Wagner <jozef.wagner@gmail.com>
2019-04-23 21:12:00 +0200

    Upgrade to JVM 11

--- metrics-clojure-graphite/project.clj
+++ metrics-clojure-graphite/project.clj
@@ -1,7 +1,7 @@
-(defproject metrics-clojure-graphite "3.0.0-SNAPSHOT"
+(defproject wagjo/metrics-clojure-graphite "3.0.0-dre-SNAPSHOT"
   :description "Graphite reporter integration for metrics-clojure"
   :url "https://github.com/sjl/metrics-clojure"
   :license {:name "MIT"}
   :profiles {:dev {:global-vars {*warn-on-reflection* true}}}
-  :dependencies [[metrics-clojure "3.0.0-SNAPSHOT"]
-                 [io.dropwizard.metrics/metrics-graphite "3.2.2"]])
+  :dependencies [[metrics-clojure "2.10.0"]
+                 [io.dropwizard.metrics/metrics-graphite "4.0.5"]])
--- metrics-clojure-jvm/project.clj
+++ metrics-clojure-jvm/project.clj
@@ -1,7 +1,7 @@
-(defproject metrics-clojure-jvm "3.0.0-SNAPSHOT"
+(defproject wagjo/metrics-clojure-jvm "3.0.0-dre-SNAPSHOT"
   :description "Gluing together metrics-clojure and jvm instrumentation."
   :url "https://github.com/sjl/metrics-clojure"
   :license {:name "MIT"}
   :profiles {:dev {:global-vars {*warn-on-reflection* true}}}
-  :dependencies [[metrics-clojure "3.0.0-SNAPSHOT"]
-                 [io.dropwizard.metrics/metrics-jvm "3.2.2"]])
+  :dependencies [[metrics-clojure "2.10.0"]
+                 [io.dropwizard.metrics/metrics-jvm "4.0.5"]])
